e0e64aee95185bffd3309e5e820b4a40eb16bd2f
Alexandre Ntougas <alexandrentougas@gmail.com>
2018-02-27 14:07:07 +0100

    put css at the right place

--- index.php
+++ index.php
@@ -4,45 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style media="screen">
-    #todo, #done {
-      min-height: 20px;
-    }
-    #todo div, #done div {
-      min-height: 20px;
-      cursor: move;
-      cursor: grab;
-      cursor: -moz-grab;
-      cursor: -webkit-grab;
-    }
-    #todo div:active, #done div:active {
-      cursor: grabbing;
-      cursor: -moz-grabbing;
-      cursor: -webkit-grabbing;
-    }
-    .gu-mirror {
-      position: fixed !important;
-      margin: 0 !important;
-      z-index: 9999 !important;
-      opacity: 0.8;
-      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=80)";
-      filter: alpha(opacity=80);
-      }
-    .gu-hide {
-      display: none !important;
-      }
-    .gu-unselectable {
-      -webkit-user-select: none !important;
-      -moz-user-select: none !important;
-      -ms-user-select: none !important;
-      user-select: none !important;
-      }
-    .gu-transit {
-      opacity: 0.2;
-      -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=20)";
-      filter: alpha(opacity=20);
-    }
-    </style>
+    <link rel="stylesheet" href="main.css">
   </head>
   <body>
     <fieldset id="list">
